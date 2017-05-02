edfee3d0977e9943aeabbba1dbe4ac22a3f57267
irishir <semplice1993@gmail.com>
2017-05-02 23:38:03 +0400

    Add files via upload

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -24,14 +24,16 @@ function renderPortfolio(data) {
     var nav = '';
     for (var i = 0; i < data["portfolio"].length; i++) {
         var img = data["portfolio"][i]["picture"]["index"],
-            title = data["portfolio"][i]["name"]["title"];
-            year = data["portfolio"][i]["name"]["year"];
+            title = data["portfolio"][i]["name"]["title"],
+            year = data["portfolio"][i]["name"]["year"],
+            about_me = data["portfolio"][i]["name"]["about_me"];
         html += '<div class="col-lg-4 block-portfolio" onclick="myModal(this.id)" id="' + [i] + '"> ';
         html += '<img class="block-portfolio__img" src="' + img + '" title="' + title + '"/>';
         html += '<p class="block-portfolio__title">' + title + '</p>';
         html += '<p class="block-portfolio__yaer">' + year + '</p>';
         html += '</div>';
         nav += '<li onclick="myModal(this.id)" id="' + [i] + '">'+ title + '</li>';
+        nav += '<li class="block-portfolio__about_me" onclick="myModal(this.id)" id="' + [i] + '">'+ about_me + '</li>';
     }
     document.getElementById("portfolio").innerHTML = html;
     document.getElementById("nav-bar").innerHTML = nav;
