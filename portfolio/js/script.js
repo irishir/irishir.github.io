bad86b6fb458027d3380cf8e58fa35b5b61f7635
irishir <semplice1993@gmail.com>
2017-05-03 09:38:37 +0400

    Add files via upload

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -81,7 +81,7 @@ function closeModal() {
     document.getElementById("Modal").style.display = "none";
 }
 
-jQuery(document).on('keyup',function(e) {
+$(document).on('keyup',function(e) {
     if (e.keyCode == 27) {
         document.getElementById("Modal").style.display = "none";
     }
